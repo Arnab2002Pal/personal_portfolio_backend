5d324a5d2d321d21877223ccd8c8c083d83146d8
ARNAB <arnabpal2002@gmail.com>
2023-07-22 18:45:56 +0530

    update -5

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import path  from "path"
-import cors from "cors"
 export const app = express()
-
-app.use(cors());
 
 app.use(express.json({
     limit: "50mb"
